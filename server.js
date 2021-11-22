254a88034894e93a71a3f7b308734b54b19f5712
Joachim <jolaf3003@gmail.com>
2021-11-22 19:17:42 +0100

    Lavet register.ejs og ændringer i mine andre filer

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const app = express()
 
 app.set('view-engine', 'ejs')
 app.get('/', (req, res) => {
-    res.render('index.ejs', { name: 'Kyle'})
+    res.render('index.ejs', { name: 'Joachim'})
 })
 
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
 app.listen (3456)
