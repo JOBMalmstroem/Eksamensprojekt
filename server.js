13ee5f937a207adb1974bc9cdfad8bdb727ed069
Joachim <jolaf3003@gmail.com>
2021-12-09 13:43:50 +0100

    Opdateret kommentarer til kode

--- server.js
+++ server.js
@@ -2,25 +2,26 @@ if (process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
 }
 
-const express = require('express')
+const express = require('express') 
 const app = express()
 const bcrypt = require('bcrypt')
 const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
 const methodOverride = require('method-override')
+//Sætter de forselige externe moduler op, samt express og laver en app.
 
-const initializePassport = require('./passport-config')
+const initializePassport = require('./passport-config') //Bruger funktionen jeg har lavet i passport-config
 const { name } = require('ejs')
 initializePassport(
     passport,
     email => users.find(user => user.email === email),
     id => users.find(user=> user.id === id)
-)
-const fs = require("fs")
+) 
+const fs = require("fs") //sætter fs op
 
-const users = []
-const products = []
+const users = [] //laver mit users objekt
+const products = [] // laver mit products array
 
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false}))
@@ -33,6 +34,7 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(methodOverride('_method'))
+//fortæller hvilke moduler, der skal bruges på appen.
 
 // laver funktion der omdanner min users data til JSON
 const data = (() => {
@@ -55,10 +57,10 @@ const pData =  (() => {
 })
 
 app.get('/', checkAuthenticated, (req, res) => {
-    res.render('index.ejs', { name: req.user.name }) // 
+    res.render('index.ejs', { name: req.user.name }) // laver route til index siden
 })
 
-app.get('/login', checkNotAuthenticated, (req, res) => { // 
+app.get('/login', checkNotAuthenticated, (req, res) => { // laver route til login siden
     res.render('login.ejs')
 })
 
@@ -66,11 +68,11 @@ app.post('/login', checkNotAuthenticated, (passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/login',
     failureFlash: true
-})))
+}))) // giver login funktionen noget hjerne
 
 app.get('/register', checkNotAuthenticated, (req, res) => {
     res.render('register.ejs')
-})
+}) // laver route til register
 
 app.post('/register', checkNotAuthenticated, async (req, res) => {
     try {
@@ -87,12 +89,12 @@ app.post('/register', checkNotAuthenticated, async (req, res) => {
     }
     data()
     
-})
+}) //skaber brugeren når der bliver oprettet en bruger, og pusher til JSON
 
 app.delete('/logout', (req, res) => {
     req.logOut()
     res.redirect('/login')
-})
+}) // via passport, blir brugeren logget af, det er logOut der gør dette
 
 app.delete('/', (req,res) => {
     var indexId = req.user.id
@@ -104,11 +106,11 @@ app.delete('/', (req,res) => {
       }
     req.logOut()
     res.redirect('/login')
-})
+}) // laver et for loop, der sletter den specifikke bruger
 
 app.get('/profile', checkAuthenticated, (req, res) => {
     res.render("profile.ejs")
-})
+}) // router til profil siden, hvor det er muligt at ændre sit brugernavn
 
 app.put('/profile', checkAuthenticated, (req,res) => {
     var indexId = req.user.id
@@ -119,12 +121,12 @@ app.put('/profile', checkAuthenticated, (req,res) => {
         }
       }
       res.redirect('/')
-})
+}) // sætter funktionen op der gør det muligt at ændre sit navn, via et for loop, lidt ala det der blev gjort i delete metoden.
 
 
 app.get('/sales/opret', checkAuthenticated, (req, res) => {
     res.render("opret.ejs")
-})
+}) // router til sales, opret salg side.
 
 app.post('/sales/opret', checkAuthenticated, (req, res) => {
         products.push({
@@ -136,7 +138,7 @@ app.post('/sales/opret', checkAuthenticated, (req, res) => {
         })
         pData()
         res.redirect('/sales')
-})
+}) // giver opret funktionen hjerne, så der nu bliver oprettet en vare, og pushet til JSON
 app.get('/sales', checkAuthenticated, (req, res) => {
     res.render("sales.ejs", { 
         products: products
@@ -148,7 +150,7 @@ app.get('/sales/:id', checkAuthenticated, (req, res) => {
     console.log(productId)
     res.render("product.ejs", { 
     })
-})
+}) //router til det specifikke salgs id, hvor det er min products.ejs fil der bliver vist som side
 
 app.put('/sales/:id', checkAuthenticated, (req,res) => {
 try {
@@ -164,14 +166,14 @@ try {
 }
 catch {
     res.status(400).redirect('/')
-} 
+} // laver en funktion, der pusher en opdateret vare, ind i products objeketet
 
 })
 
 app.delete('/sales', (req,res) => {
     products.splice(0,products.length)
     res.status(200).redirect('/sales')
-})
+}) // laver en funktion der sletter salget.
 
 app.get('/kategori/:kategori', (req, res) => {
     const kategorier = products.find(k=> k.kategori === req.params.kategori)
@@ -179,7 +181,7 @@ app.get('/kategori/:kategori', (req, res) => {
     return res.send("Forkert kategori")
     res.render('kategori.ejs' , {kategorier: kategorier})
 })
-
+//laver en route der viser en tabel over den individuelle kategori, så varene bliver opdelt efter kategeori
 
 function checkAuthenticated(req,res, next) {
     if (req.isAuthenticated()) {
@@ -187,12 +189,12 @@ function checkAuthenticated(req,res, next) {
 
     }
     res.redirect('/login')
-}
+} // tjekker om brugeren, er autentificeret
 
 function checkNotAuthenticated(req, res, next) {
     if(req.isAuthenticated()){
        return res.redirect('/')
     }
-    next()
-}
-module.exports = app.listen (3456) 
+    next() 
+}// tjekker om brugeren ej er autentificeret
+module.exports = app.listen (3456) //fortæller appen, på hvilken port den skal lytte, samtidig exporter jeg modulet, da jeg bruger det i min unittest.
--- test/unitTest.js
+++ test/unitTest.js
@@ -2,18 +2,18 @@ const {expect} = require("chai")
 const chai = require("chai")
 const chaiHttp = require("chai-http")
 const server = require("../server")
-
+// bruger de nødvendige moduler, samt serveren, da det er her testen skal finde sted.
 chai.use(chaiHttp)
 
-describe ("Tester første krav", () => {
+describe ("Tester første krav", () => { // laver unit test funktionenerne 
     describe ("POST/register", () => {
 
         it("Skal registerer en user", (done) => {
-            const user = {
-            id: Date.now().toString(),
-            name: "Bruger",
-            email: "Bruger@Mail.com",
-            password : "$2b$10$3c4FAh80Vyzw/P47L07xV.YLPwpO3xrC0ESl0Zl3mb4F9xDt9FEj."
+            const user = { // opretter en bruger
+            id: Date.now().toString(), //giver random id
+            name: "Bruger", // sætter navn til "Bruger"
+            email: "Bruger@Mail.com", //sætter mail til "Bruger@Mail.com"
+            password : "$2b$10$3c4FAh80Vyzw/P47L07xV.YLPwpO3xrC0ESl0Zl3mb4F9xDt9FEj." //hashed password
             }
             chai
             .request(server)
@@ -22,11 +22,11 @@ describe ("Tester første krav", () => {
             .end((err, res) => {
                 expect(err).to.be.null;
                 expect(res.status).to.equal(200)
-                expect(res.body).to.be.an("object")
-                expect(res.body).to.not.be.an("array")
-                expect(user).to.have.property("name").eq("Bruger")
-                expect(user).to.have.property("email").eq("Bruger@Mail.com")
-                expect(user).to.have.property("password").eq("$2b$10$3c4FAh80Vyzw/P47L07xV.YLPwpO3xrC0ESl0Zl3mb4F9xDt9FEj.")
+                expect(res.body).to.be.an("object") //forventer at bodyen er et objekt
+                expect(res.body).to.not.be.an("array") //forventer ikke det er et array
+                expect(user).to.have.property("name").eq("Bruger") // forventer navnet på user er lig det jeg satte det som
+                expect(user).to.have.property("email").eq("Bruger@Mail.com") // forventer mail er lig det jeg satte den som
+                expect(user).to.have.property("password").eq("$2b$10$3c4FAh80Vyzw/P47L07xV.YLPwpO3xrC0ESl0Zl3mb4F9xDt9FEj.") //forventer password er det samme som fortalt
                 done()
             })  
         })
